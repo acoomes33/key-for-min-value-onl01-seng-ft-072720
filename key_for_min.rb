d9eeeb3b5f3603ccf51a3c936bce3523f28084d2
Alan Coomes <acoomes1@cord.edu>
2020-08-07 20:02:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash.map do |name, value|
-    value <=> value
+def key_for_min_value(hash)
+  min_value = hash[1]
+  min_key = hash[0]
+  hash.each do |key, value|
+  
+    end
+  return min_key
 end
 
-min_valu 
-min_key
-hash.each do |word| nd 
-retirn min_key
-we need to loop through the hash (we can just use each) and for each loop, compare the current value to our current min_value. If the current value is lower than that value, our min_value is no longer our min_value, because we just found a lower value than that. We can reset our min_value to the value we're looking at right now. And we can also update our min_key at this point too, because the min_key is just the key that goes with the min_value
-after the loop, our min_key and min_value variables will actually hold the right values and we can return min_key
+
+# we need to loop through the hash (we can just use each) and for each loop, compare the current value to our current min_value. If the current value is lower than that value, our min_value is no longer our min_value, because we just found a lower value than that. We can reset our min_value to the value we're looking at right now. And we can also update our min_key at this point too, because the min_key is just the key that goes with the min_value
+# after the loop, our min_key and min_value variables will actually hold the right values and we can return min_key
