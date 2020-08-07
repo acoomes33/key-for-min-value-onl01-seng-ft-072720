3c0a4aa462207fc63305c358c06907bee5f02ab2
Alan Coomes <acoomes1@cord.edu>
2020-08-07 20:05:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,6 +13,3 @@ def key_for_min_value(hash)
   return min_key
 end
 
-
-# we need to loop through the hash (we can just use each) and for each loop, compare the current value to our current min_value. If the current value is lower than that value, our min_value is no longer our min_value, because we just found a lower value than that. We can reset our min_value to the value we're looking at right now. And we can also update our min_key at this point too, because the min_key is just the key that goes with the min_value
-# after the loop, our min_key and min_value variables will actually hold the right values and we can return min_key
